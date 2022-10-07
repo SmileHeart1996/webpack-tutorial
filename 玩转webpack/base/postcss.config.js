45358af7985a3a9bc4f830d03d00c8af0842af63
SmileHeart1996 <Xie_Kang1996@163.com>
2022-10-07 19:08:51 +0800

    单位转换

--- 玩转webpack/base/postcss.config.js
+++ 玩转webpack/base/postcss.config.js
@@ -1,7 +1,29 @@
+const PostcssPxToViewport = require('postcss-px-to-viewport');
+
 module.exports = {
     plugins: [
+        "postcss-flexbugs-fixes",
         [
-            "postcss-preset-env"
-        ]
+            "postcss-preset-env",
+            {
+                autoprefixer: {
+                    flexbox: 'no-2009',
+                },
+                stage: 3,
+            }
+        ],
+        [
+            "postcss-px-to-viewport",
+            {
+                viewportWidth: 1920,
+                viewportHeight: 1080,
+                unitPrecision: 3,
+                viewportUnit: "vw",
+                selectorBlackList: [],
+                minPixelValue: 1,
+                mediaQuery: false,
+            },
+        ],
+        "postcss-normalize",
     ]
 }
--- 玩转webpack/base/webpack.dev.js
+++ 玩转webpack/base/webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -24,7 +25,7 @@ module.exports = {
             },
             {
                 test: /\.less$/,
-                use: ['style-loader', 'css-loader', 'less-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
@@ -34,6 +35,9 @@ module.exports = {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
         new CleanWebpackPlugin(),
     ],
     devServer: {
